a59e3235be9c25dda09c0292ef4b10c2f76ca67a
ajay <ajay.algot1993@gmail.com>
2017-10-24 19:48:46 +0530

    modules completed

---
+++ src/main/java/com/niit/configuraton/WebAppInitializer.java
@@ -0,0 +1,24 @@
+package com.niit.configuraton;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+	
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+	return new Class[]{DBConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+	return new Class[]{WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+	return new String[]{"/"};
+
+	}
+
+	}
